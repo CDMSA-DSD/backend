502d06a93e801be54884b36000b397f9f649ad19
David <118130226+dfabbroni@users.noreply.github.com>
2025-11-19 00:05:45 +0100

    merge old org controller with new for github integrations

--- src/main/java/dsd/api/cdmsa/controller/OrgController.java
+++ src/main/java/dsd/api/cdmsa/controller/OrgController.java
@@ -27,6 +27,24 @@ public class OrgController {
     private OrgModelAssembler orgModelAssembler;
     private PagedResourcesAssembler<Organization> pagedResourcesAssembler;
 
+    private UserPrincipal getAuthenticatedPrincipal() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserPrincipal) {
+            return (UserPrincipal) principal;
+        }
+        throw new UserNotFoundException("anonymous");
+    }
+
+    private Long getCurrentUserId(HttpServletRequest request) {
+        try {
+            return getAuthenticatedPrincipal().getUser().getId();
+        } catch (UserNotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new UserNotFoundException("anonymous");
+        }
+    }
+
     // GET Org (individual)
     @GetMapping(value = "/{id}")
     public ResponseEntity<EntityModel<OrganizationResponse>> getOrg(@PathVariable Long id) {
@@ -44,4 +62,56 @@ public class OrgController {
         return ResponseEntity.ok(pagedResourcesAssembler.toModel(orgs, orgModelAssembler));
     }
 
+    // Get organization details
+    @GetMapping
+    public ResponseEntity<OrganizationResponse> getOrgDetails(HttpServletRequest httpRequest) {
+        Long userId = getCurrentUserId(httpRequest);
+        OrganizationResponse org = orgService.getOrgDetails(userId);
+        return ResponseEntity.ok(org);
+    }
+
+    // Update organization details
+    @PutMapping
+    public ResponseEntity<OrganizationResponse> updateOrgDetails(@Valid @RequestBody UpdateOrganizationRequest request, HttpServletRequest httpRequest) {
+        Long userId = getCurrentUserId(httpRequest);
+        OrganizationResponse orgUpdated = orgService.updateOrgDetails(userId, request);
+        return ResponseEntity.ok(orgUpdated);
+    }
+
+    // connect GitHub through pat
+    @PostMapping("/github")
+    public ResponseEntity<ConnectGitHubResponse> connectGitHub(@Valid @RequestBody ConnectGitHubRequest request, HttpServletRequest httpRequest){
+        // check id user is admin
+        Long userId = getCurrentUserId(httpRequest);
+        ConnectGitHubResponse resp = orgService.connectGitHub(userId, request);
+        return ResponseEntity.ok(resp);
+    }
+
+    // get the repos of the organization, to show them in a dropdown menu and select one
+    @GetMapping("/github/repos")
+    public ResponseEntity<List<RepositoryResponse>> getRepos(HttpServletRequest httpRequest) {
+        Long userId = getCurrentUserId(httpRequest);
+        return ResponseEntity.ok(orgService.getRepos(userId));
+    }
+
+    // get the branches of a certain repo of the organization, to show them in a dropdown menu and select one
+    // check if passing owner is necessary, we have only one owner?
+    @GetMapping("/github/{owner}/{repo}/branches") // owner and repo sent from the frontend, we have them from getRepos
+    public ResponseEntity<List<BranchResponse>> getBranches(@PathVariable String owner,
+                                                            @PathVariable String repo,
+                                                            HttpServletRequest httpRequest) {
+        Long userId = getCurrentUserId(httpRequest);
+        return ResponseEntity.ok(orgService.getBranches(userId, owner, repo));
+    }
+
+    // choose repo e branch where to store ADRs
+    @PutMapping("/github/selection")
+    public ResponseEntity<OrganizationResponse> saveRepoBranchSelection(
+            @Valid @RequestBody RepoBranchSelectionRequest request,
+            HttpServletRequest httpRequest) {
+
+        Long userId = getCurrentUserId(httpRequest);
+        OrganizationResponse response = orgService.saveRepoBranchSelection(userId, request);
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/dsd/api/cdmsa/service/OrgService.java
+++ src/main/java/dsd/api/cdmsa/service/OrgService.java
@@ -84,23 +84,6 @@ public class OrgService {
         throw new OrgExistsException(org.companyName());
     }
 
-    @Transactional(readOnly = true)
-    public Organization getOrgDetails(Long orgId) {
-        return orgRepo.findById(orgId).orElseThrow(() -> new OrgNotFoundException(orgId));
-    }
-
-    @Transactional
-    public Organization updateOrgDetails(Long orgId, UpdateOrganizationRequest request) {
-        Organization org = getOrgDetails(orgId);
-
-        org.setName(request.companyName().trim());
-        org.setDescription(request.description().trim());
-        org.setDomain(request.domain().trim());
-        // org.setGitHubToken(request.gitHubToken().trim());
-
-        return orgRepo.save(org);
-    }
-
     public void deleteOrg(Long id) {
         orgRepo.deleteById(id);
     }
@@ -110,4 +93,158 @@ public class OrgService {
         return orgRepo.findById(orgId, pageable);
     }
 
+    @Transactional(readOnly = true)
+    public OrganizationResponse getOrgDetails(Long id) {
+
+        User user = userRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Organization org = user.getOrg();
+
+        // when github is not connected yet, the last three are null and so not displayed in the UI
+        return new OrganizationResponse(
+                org.getName(),
+                org.getDescription(),
+                org.getDomain(),
+                org.getSelectedRepoName(),
+                org.getSelectedBranchName(),
+                org.getRepoOwner()
+        );
+    }
+
+    @Transactional
+    public OrganizationResponse updateOrgDetails(Long id, UpdateOrganizationRequest request) {
+
+        User user = userRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Organization org = user.getOrg();
+
+        org.setName(request.companyName().trim());
+        org.setDescription(request.description().trim());
+        org.setDomain(request.domain().trim());
+        org.setSelectedRepoName(request.selectedRepoName() != null ? request.selectedRepoName().trim() : null);
+        org.setSelectedBranchName(request.selectedBranchName() != null ? request.selectedBranchName().trim() : null);
+
+        Organization updatedOrg = orgRepo.save(org);
+        return new OrganizationResponse(
+                updatedOrg.getName(),
+                updatedOrg.getDescription(),
+                updatedOrg.getDomain(),
+                updatedOrg.getSelectedRepoName(),
+                updatedOrg.getSelectedBranchName(),
+                updatedOrg.getRepoOwner()
+        );
+    }
+
+    @Transactional
+    public ConnectGitHubResponse connectGitHub (Long userId, ConnectGitHubRequest request){
+        User user = userRepo.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Organization org = user.getOrg();
+
+        String pat = request.pat().trim();
+
+        // call github API to retrieve the user (organization) account from the pat
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + pat);
+        headers.set("Accept", "application/vnd.github.v3+json");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<JsonNode> response;
+        try {
+            response = restTemplate.exchange(
+                    "https://api.github.com/user",
+                    HttpMethod.GET,
+                    entity,
+                    JsonNode.class
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid PAT or unable to contact GitHub");
+        }
+
+        JsonNode body = response.getBody();
+        String githubLogin = body.get("login").asText();
+        // name probably not necessary
+        String githubName = body.has("name") && !body.get("name").isNull() ? body.get("name").asText() : githubLogin;
+
+        // TODO: encrypt PAT before storing
+        org.setGitHubToken(pat);
+        org.setRepoOwner(githubLogin);
+        orgRepo.save(org);
+
+        return new ConnectGitHubResponse(githubLogin, githubName);
+    }
+
+    @Transactional(readOnly = true)
+    public List<RepositoryResponse> getRepos(Long userId) {
+        User user = userRepo.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Organization org = user.getOrg();
+
+        String pat = org.getGitHubToken();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + pat);
+        headers.set("Accept", "application/vnd.github.v3+json");
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<JsonNode[]> response = restTemplate.exchange(
+                "https://api.github.com/user/repos",
+                HttpMethod.GET,
+                entity,
+                JsonNode[].class
+        );
+
+        return Arrays.stream(response.getBody())
+                .map(repo -> new RepositoryResponse(repo.get("name").asText(),
+                        repo.get("owner").get("login").asText()))
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<BranchResponse> getBranches(Long userId, String owner, String repo) {
+        User user = userRepo.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Organization org = user.getOrg();
+        String pat = org.getGitHubToken();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + pat);
+        headers.set("Accept", "application/vnd.github.v3+json");
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<JsonNode[]> response = restTemplate.exchange(
+                "https://api.github.com/repos/" + owner + "/" + repo + "/branches",
+                HttpMethod.GET,
+                entity,
+                JsonNode[].class
+        );
+
+        return Arrays.stream(response.getBody())
+                .map(b -> new BranchResponse(b.get("name").asText()))
+                .toList();
+    }
+
+    @Transactional
+    public OrganizationResponse saveRepoBranchSelection(Long userId, RepoBranchSelectionRequest request) {
+        User user = userRepo.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Organization org = user.getOrg();
+
+        org.setSelectedRepoName(request.repoName().trim());
+        org.setSelectedBranchName(request.branchName().trim());
+
+        orgRepo.save(org);
+
+        return new OrganizationResponse(
+                org.getName(),
+                org.getDescription(),
+                org.getDomain(),
+                org.getSelectedRepoName(),
+                org.getSelectedBranchName(),
+                org.getRepoOwner()
+        );
+    }
 }
