a90a7cd4ed195c1bb45d29943af9e1f805bf93d6
mruizcas04 <mruizcas04@gmail.com>
2025-11-18 12:26:38 +0100

    user is now identified and added by its email, added creation date to contexts

--- src/main/java/dsd/api/cdmsa/controller/ContextController.java
+++ src/main/java/dsd/api/cdmsa/controller/ContextController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/contexts")
 @RequiredArgsConstructor
 public class ContextController {
